ec7c77794826cca969692bbbb1606705349b3845
leekihyun <21300506@handong.edu>
2020-04-17 16:54:56 +0900

    gitignore apply

---
+++ record.h
@@ -0,0 +1,14 @@
+#ifndef _RECORD_H_
+#define _RECORD_H_
+
+// type defition
+typedef struct{
+  char name[100] ;
+  char country[100];
+  char position[100];
+  int age;
+	// TODO: define your own structure type here
+  //struct Record *next;
+} Record ;
+
+#endif
